e63687f6037922a866e032d2a0de177e31b6ad33
Neelam Makwana <142253620+neelammkw@users.noreply.github.com>
2023-12-08 19:17:47 +0530

    changes

--- script.js
+++ script.js
@@ -1,63 +1,105 @@
 console.log("Tic tac toe");
 let music = new Audio("free-sound-1674743533.mp3");
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
- let turn = "X";
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
 let isgameover = false;
 
-const changeTurn = ()=>{
-    return turn === "X" ? "0" : "X";
-}
-//Function to check win
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
 
-const checkWin = () =>{
-let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
-            isgameover = true;
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
-        }
-    })
+// Declare mediaQuery outside of the checkWin function
+const mediaQuery = window.matchMedia("(max-width: 786px)");
+
+// Function to set styles
+function setStyles(e) {
+
+    
+  const lineElement = document.querySelector(".line");
+
+  if (mediaQuery.matches) {
+    // Apply styles for viewports 768px or less
+    lineElement.style.width = "50vw";
+
+    lineElement.style.transform = `translate(${e[3]}vw, ${e[4] + 13}vw) rotate(${e[5]}deg)`;
+    console.log("Current value:", wins[0][4]);
+
+ const indexToChange = 4; // index of the value to change in the sub-array
+    const newValue = "10vw"; // new value as a string or a number
+
+    wins[0][indexToChange] = newValue;
+
+    // Log the value after changing
+    console.log("New value:", wins[0][4]);
+
+    
+  } else {
+    // Apply default styles for larger viewports
+    lineElement.style.width = "20vw";
+  }
 }
 
-//Game Logi
+const wins = [
+  [0, 1, 2, 5, 5, 0],
+  [3, 4, 5, 5, 15, 0],
+  [6, 7, 8, 5, 25, 0],
+  [0, 3, 6, -5, 15, 90],
+  [1, 4, 7, 5, 15, 90],
+  [2, 5, 8, 15, 15, 90],
+  [0, 4, 8, 5, 15, 45],
+  [2, 4, 6, 5, 15, 135],
+];
+
+// Function to check win
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText = boxtext[e[0]].innerText + " Won";
+      isgameover = true;
+      document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
+      document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+
+      // Initial styles setup
+      setStyles(e);
+
+      // Add a listener to react to changes in viewport width
+      mediaQuery.addListener(() => setStyles(e));
+    }
+  });
+};
+
+// Game Logic
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element => {
-    let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', (e) =>{
-        if(boxtext.innerText == ''){
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
-            checkWin();
-if(!isgameover){
-            document.getElementsByClassName("info")[0].innerText = "turn for " + turn;
-        }
-        }
-    })
-})
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText == "" && !isgameover) {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      audioTurn.play();
+      checkWin();
+      if (!isgameover) {
+        document.getElementsByClassName("info")[0].innerText = "turn for " + turn;
+      }
+    }
+  });
+});
 
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
-    });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  isgameover = false;
+  document.querySelector(".line").style.width = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+  document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px";
+});
